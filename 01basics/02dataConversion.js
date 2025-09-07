0eb4154a13cd7f2cfa4c41b749eb1687c70c75d9
rishu774 <129262490+rishu774@users.noreply.github.com>
2025-09-07 00:54:38 +0000

    conversion of the data type

---
+++ 01basics/02dataConversion.js
@@ -0,0 +1,51 @@
+let score = "33"
+
+
+console.log(score);
+console.log(typeof score);
+console.log(typeof( score));
+
+
+let valueInNumber = Number ( score) // right here , whave assigned the value of the score as numberin goes or take the as string 
+// We have take the valueInNumbe assigned the of = Numbe (score) to it
+
+console.log(typeof  valueInNumber);
+
+
+let number= 0
+
+
+console.log(number);
+console.log(typeof number);
+console.log(typeof( number));
+
+
+let IsthevalueisNumber = Boolean (number)
+ 
+console.log(typeof IsthevalueisNumber);
+
+
+console.log(IsthevalueisNumber);
+
+// NaN :-> is know as  the "not a number " and it is Number "data type"
+
+// in boolean when  wedo conversion - it is showed as the : 1 = true and 0 = false
+ // when we used the ""(empty string ) in boolean :-> "" = false
+
+
+// when we convert a no. from number to a string 
+
+
+let randomNumber= 33
+
+let conversionToString = String (randomNumber)
+
+console.log(conversionToString);
+console.log(typeof conversionToString);
+
+// So we have converted the number into a string 
+
+
+
+
+
